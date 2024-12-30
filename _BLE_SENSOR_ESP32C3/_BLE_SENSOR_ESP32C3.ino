a8a95972d1922bf5b5c17fef614601a8b3e35219
artem <artsem.belahubau@m.sensor.by>
2024-12-30 09:11:22 +0300

    arduino sensor

--- _BLE_SENSOR_ESP32C3/_BLE_SENSOR_ESP32C3.ino
+++ _BLE_SENSOR_ESP32C3/_BLE_SENSOR_ESP32C3.ino
@@ -4,23 +4,25 @@
 #include <BTAdvertisedDevice.h>
 #include <BTScan.h>
 #include <BLE2902.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 
-#include <DHT11.h>
-
-//BLE server name
+// BLE server name
 #define bleServerName "DHT11_ESP32C3"
 
+#define DHTPIN 2 // Digital pin connected to the DHT sensor
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
 
-float temp;
-float tempF;
-float hum;
+// Uncomment the type of sensor in use:
+#define DHTTYPE DHT11 // DHT 11
+// #define DHTTYPE    DHT22     // DHT 22 (AM2302)
+// #define DHTTYPE    DHT21     // DHT 21 (AM2301)
 
-DHT11 dht(2);  // выход DAT подключен к цифровому входу 2
+DHT_Unified dht(DHTPIN, DHTTYPE);
 
-// Timer variables
-unsigned long lastTime = 0;
-unsigned long timerDelay = 30000;
-
+uint32_t delayMS;
 
 bool deviceConnected = false;
 
@@ -30,28 +32,77 @@ bool deviceConnected = false;
 #define TEMP_CHARACT_SERVICE_UUID "ccfe71ea-e98b-4927-98e2-6c1b77d1f756"
 #define NUM_CHARACT_SERVICE_UUID "6ed76625-573e-4caa-addf-3ddc5a283095"
 
-
 // Temperature Characteristic and Descriptor
 BLECharacteristic bmeTemperatureCelsiusCharacteristics(TEMP_CHARACT_SERVICE_UUID, BLECharacteristic::PROPERTY_NOTIFY);
-BLEDescriptor bmeTemperatureCelsiusDescriptor(BLEUUID((uint16_t)0x2902)); //https://bitbucket.org/bluetooth-SIG/public/src/b012e68be90070f06ca43a9e24463cb86ca817c6/assigned_numbers/uuids/descriptors.yaml?at=main#descriptors.yaml-41
+BLEDescriptor bmeTemperatureCelsiusDescriptor(BLEUUID((uint16_t)0x2902)); // https://bitbucket.org/bluetooth-SIG/public/src/b012e68be90070f06ca43a9e24463cb86ca817c6/assigned_numbers/uuids/descriptors.yaml?at=main#descriptors.yaml-41
 
 // Humidity Characteristic and Descriptor
 BLECharacteristic bmeHumidityCharacteristics(NUM_CHARACT_SERVICE_UUID, BLECharacteristic::PROPERTY_NOTIFY);
-BLEDescriptor bmeHumidityDescriptor(BLEUUID((uint16_t)0x2902));//было 0x2903 //https://bitbucket.org/bluetooth-SIG/public/src/b012e68be90070f06ca43a9e24463cb86ca817c6/assigned_numbers/uuids/descriptors.yaml?at=main#descriptors.yaml-41
+BLEDescriptor bmeHumidityDescriptor(BLEUUID((uint16_t)0x2902)); // было 0x2903 //https://bitbucket.org/bluetooth-SIG/public/src/b012e68be90070f06ca43a9e24463cb86ca817c6/assigned_numbers/uuids/descriptors.yaml?at=main#descriptors.yaml-41
 
-//Setup callbacks onConnect and onDisconnect
-class MyServerCallbacks : public BLEServerCallbacks {
-  void onConnect(BLEServer *pServer) {
+// Setup callbacks onConnect and onDisconnect
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
     deviceConnected = true;
   };
-  void onDisconnect(BLEServer *pServer) {
+  void onDisconnect(BLEServer *pServer)
+  {
     deviceConnected = false;
   }
 };
 
-void setup() {
+void initDHTSensor()
+{
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print(F("Sensor Type: "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:  "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:   "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:   "));
+  Serial.print(sensor.max_value);
+  Serial.println(F("°C"));
+  Serial.print(F("Min Value:   "));
+  Serial.print(sensor.min_value);
+  Serial.println(F("°C"));
+  Serial.print(F("Resolution:  "));
+  Serial.print(sensor.resolution);
+  Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print(F("Sensor Type: "));
+  Serial.println(sensor.name);
+  Serial.print(F("Driver Ver:  "));
+  Serial.println(sensor.version);
+  Serial.print(F("Unique ID:   "));
+  Serial.println(sensor.sensor_id);
+  Serial.print(F("Max Value:   "));
+  Serial.print(sensor.max_value);
+  Serial.println(F("%"));
+  Serial.print(F("Min Value:   "));
+  Serial.print(sensor.min_value);
+  Serial.println(F("%"));
+  Serial.print(F("Resolution:  "));
+  Serial.print(sensor.resolution);
+  Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
+}
+
+void setup()
+{
   Serial.begin(115200);
-  pinMode(2, OUTPUT);  // LED
+  // pinMode(2, OUTPUT);  // LED
 
   // Create the BLE Device
   BLEDevice::init(bleServerName);
@@ -82,42 +133,52 @@ void setup() {
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pServer->getAdvertising()->start();
   Serial.println("Waiting a client connection to notify...");
+
+  initDHTSensor();
 }
 
-void loop() {
-  if (deviceConnected) {
-    if ((millis() - lastTime) > timerDelay) {
-      int temperature = 0;
-      int humidity = 0;
-
-      // Attempt to read the temperature and humidity values from the DHT11 sensor.
-      int result = dht.readTemperatureHumidity(temperature, humidity);
-      if (result == 0) {
-        
-        static char temperatureCTemp[6];
-        dtostrf(temperature, 6, 2, temperatureCTemp);
-        //Set temperature Characteristic value and notify connected client
-        bmeTemperatureCelsiusCharacteristics.setValue(temperatureCTemp);
-        bmeTemperatureCelsiusCharacteristics.notify();
-        Serial.print("Temperature Celsius: ");
-        Serial.print(temperature);
-        Serial.print(" ºC");
-
-        //Notify humidity reading from BME
-        static char humidityTemp[6];
-        dtostrf(humidity, 6, 2, humidityTemp);
-        //Set humidity Characteristic value and notify connected client
-        bmeHumidityCharacteristics.setValue(humidityTemp);
-        bmeHumidityCharacteristics.notify();
-        Serial.print(" - Humidity: ");
-        Serial.print(humidity);
-        Serial.println(" %");
-
-        lastTime = millis();
-      } else {
-        // Print error message based on the error code.
-        Serial.println(DHT11::getErrorString(result));
-      }
+void loopDHT()
+{
+  // Delay between measurements.
+  delay(delayMS);
+  // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature))
+  {
+    Serial.println(F("Error reading temperature!"));
+  }
+  else
+  {
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+    bmeTemperatureCelsiusCharacteristics.setValue(event.temperature);
+    if (deviceConnected)
+    {
+      bmeTemperatureCelsiusCharacteristics.notify();
     }
   }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity))
+  {
+    Serial.println(F("Error reading humidity!"));
+  }
+  else
+  {
+    Serial.print(F("Humidity: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+    bmeHumidityCharacteristics.setValue(event.relative_humidity);
+    if (deviceConnected)
+    {
+      bmeHumidityCharacteristics.notify();
+    }
+  }
+}
+
+void loop()
+{
+  loopDHT();
 }
